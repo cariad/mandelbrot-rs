de5787a9c23d0a2be7962fde1e7ef4292c930e93
Cariad Eccleston <cariad@cariad.earth>
2022-10-01 12:01:54 +0100

    Add SDL2

---
+++ build.rs
@@ -0,0 +1,65 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2013 Mozilla Foundation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    let target = env::var("TARGET").unwrap();
+    if target.contains("pc-windows") {
+        let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+        let mut lib_dir = manifest_dir.clone();
+        let mut dll_dir = manifest_dir.clone();
+        if target.contains("msvc") {
+            lib_dir.push("msvc");
+            dll_dir.push("msvc");
+        }
+        else {
+            lib_dir.push("gnu-mingw");
+            dll_dir.push("gnu-mingw");
+        }
+        lib_dir.push("lib");
+        dll_dir.push("dll");
+        if target.contains("x86_64") {
+            lib_dir.push("64");
+            dll_dir.push("64");
+        }
+        else {
+            lib_dir.push("32");
+            dll_dir.push("32");
+        }
+        println!("cargo:rustc-link-search=all={}", lib_dir.display());
+        for entry in std::fs::read_dir(dll_dir).expect("Can't read DLL dir")  {
+            let entry_path = entry.expect("Invalid fs entry").path();
+            let file_name_result = entry_path.file_name();
+            let mut new_file_path = manifest_dir.clone();
+            if let Some(file_name) = file_name_result {
+                let file_name = file_name.to_str().unwrap();
+                if file_name.ends_with(".dll") {
+                    new_file_path.push(file_name);
+                    std::fs::copy(&entry_path, new_file_path.as_path()).expect("Can't copy from DLL dir");
+                }
+            }
+        }
+    }
+}
