98572f2de46b769bbaeaaf56c324f7d10b5fb1ac
Cariad Eccleston <cariad@cariad.earth>
2022-10-01 13:26:06 +0100

    Open a window (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,32 @@
+pub mod vector2;
+
+use sdl2::event::Event;
+use std::time::Duration;
+use vector2::Vector2;
+
 fn main() -> Result<(), String> {
-    println!("TODO: Mandelbrot set goes here");
+    let win_size = Vector2 { x: 800, y: 600 };
+
+    let sdl = sdl2::init()?;
+    let video = sdl.video()?;
+
+    let _window = video
+        .window("mandelbrot-rs", win_size.x, win_size.y)
+        .position_centered()
+        .build()
+        .expect("initialising SDL video subsystem");
+
+    let mut event_pump = sdl.event_pump()?;
+
+    'event_loop: loop {
+        for event in event_pump.poll_iter() {
+            if let Event::Quit { .. } = event {
+                break 'event_loop;
+            }
+        }
+
+        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+
     Ok(())
 }
---
+++ src/vector2.rs
@@ -0,0 +1,4 @@
+pub struct Vector2 {
+    pub x: u32,
+    pub y: u32,
+}
