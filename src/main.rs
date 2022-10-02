30f146b8455b40c4a47a627c87da18631475bc2e
Cariad Eccleston <cariad@cariad.earth>
2022-10-02 13:35:49 +0100

    Render to a texture

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ pub mod loupe;
 pub mod vector2;
 
 use loupe::Loupe;
-use sdl2::{event::Event, pixels::Color, rect::Point};
-use std::{cmp::max, time::Duration};
+use sdl2::pixels::PixelFormatEnum;
+use sdl2::render::TextureAccess;
+use sdl2::{event::Event, pixels::Color};
+use std::cmp::max;
 use vector2::Vector2;
 
 fn main() -> Result<(), String> {
@@ -27,7 +29,22 @@ fn main() -> Result<(), String> {
         .build()
         .expect("building window");
 
-    let mut canvas = window.into_canvas().build().expect("building canvas");
+    let mut canvas = window
+        .into_canvas()
+        .accelerated()
+        .build()
+        .expect("building canvas");
+
+    let texture_creator = canvas.texture_creator();
+
+    let mut man_texture = texture_creator
+        .create_texture(
+            PixelFormatEnum::RGB888,
+            TextureAccess::Streaming,
+            win_size.x,
+            win_size.y,
+        )
+        .expect("creating texture");
 
     let loupe = Loupe::new();
 
@@ -35,29 +52,6 @@ fn main() -> Result<(), String> {
         x: (win_size.x as f32 / win_max_len) * 0.5,
         y: (win_size.y as f32 / win_max_len) * 0.5,
     };
-
-    for x in 0..win_size.x {
-        for y in 0..win_size.y {
-            // Percentage distance from the centre of the canvas.
-            let pc = Vector2 {
-                x: (x as f32 / win_max_len - offset.x),
-                y: (y as f32 / win_max_len - offset.y),
-            };
-
-            let iterations = loupe.iterations(&pc, max_iterations);
-
-            let color = if iterations > 30 {
-                Color::RGB(0, 0, 0)
-            } else {
-                Color::RGB(255, 255, 255)
-            };
-
-            canvas.set_draw_color(color);
-            canvas.draw_point(Point::new(x as i32, y as i32))?;
-        }
-    }
-
-    canvas.present();
 
     let mut event_pump = sdl.event_pump()?;
     let mut frame_count: u8 = 0;
@@ -69,6 +63,36 @@ fn main() -> Result<(), String> {
             }
         }
 
+        man_texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
+            for x in 0..win_size.x {
+                for y in 0..win_size.y {
+                    // Percentage distance from the centre of the canvas.
+                    let pc = Vector2 {
+                        x: (x as f32 / win_max_len - offset.x),
+                        y: (y as f32 / win_max_len - offset.y),
+                    };
+
+                    let iterations = loupe.iterations(&pc, max_iterations);
+
+                    let color = if iterations > 30 {
+                        Color::RGB(0, 0, 0)
+                    } else {
+                        Color::RGB(255, 255, 255)
+                    };
+
+                    let offset = ((y as usize) * pitch) + ((x as usize) * 4);
+
+                    buffer[offset] = color.b;
+                    buffer[offset + 1] = color.g;
+                    buffer[offset + 2] = color.r;
+                    buffer[offset + 3] = 255;
+                }
+            }
+        })?;
+
+        canvas.copy(&man_texture, None, None)?;
+        canvas.present();
+
         last = now;
         now = timer.performance_counter();
         delta = ((now - last) * 1000) as f32 / performance_frequency;
@@ -78,8 +102,6 @@ fn main() -> Result<(), String> {
             frame_count = 0;
             println!("delta={delta}");
         }
-
-        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 
     Ok(())
